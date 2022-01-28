a4071f6f9dc54d55243c1838f2ef3ac41459a456
NanNaDa <mhilt767@gmail.com>
2022-01-28 22:59:43 +0900

    add event

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,7 @@
 let myHeading = document.querySelector('h1');
 myHeading.textContent = 'Hello world!';
 
-
+document.querySelector('html').onclick = function() {
+    alert('Ouch! Stop poking me!';)
+}
 
