b806580ee3c71723419d2a5d99c10a9ff451bf50
NanNaDa <mhilt767@gmail.com>
2022-01-28 23:09:50 +0900

    change image

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,10 @@
-let myHeading = document.querySelector('h1');
-myHeading.textContent = 'Hello world!';
+let myImage = document.querySelector('img')
 
-document.querySelector('html').onclick = function() {
-    alert('Ouch! Stop poking me!');
+myImage.onclick = function() {
+    let mySrc = myImage.getAttribute('src');
+    if (mySrc === 'images/firefox-icon.png') {
+        myImage.setAttribute('src', 'images/box-model.png');
+    } else {
+        myImage.setAttribute('src', 'images/firefox-icon.png')
+    }
 }
-
